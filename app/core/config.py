76eaee4d4fe8bafda10d0d800a92b0efeb0bf6ac
GasStationManager <GasStationCodeManager@gmail.com>
2024-11-13 09:35:41 +0000

    adjust postgresql username to fit with README instruction

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:LocalPass@localhost/coding_challenge_db"
+    DATABASE_URL: str = "postgresql://coding_challenge_user:LocalPass@localhost/coding_challenge_db"
     # Add other configuration variables here
     SECRET_KEY: str = "GasStationSecret"  # Change this!
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
